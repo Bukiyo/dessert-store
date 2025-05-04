cc4a4d0bbf1506d4f7f927d72eb1c55542305bac
Bukola Olayinka <Buky96@yahoo.com>
2025-05-03 22:09:53 -0600

    Fixed confirm modal's width extending behaviour on smaller screens. I simply removed the width property as this allowed the section maintain the width of its container

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
-import VConsole from 'vconsole';
-
-// Initialize vConsole in development only
-if (process.env.NODE_ENV === 'development') {
-  // will show a tabbed console in mobile Safari/Chrome
-  /* eslint-disable no-new */
-  new VConsole();
-}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
